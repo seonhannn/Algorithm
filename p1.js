4e37e38464fe4461ce1dd99c12a09fbe8f4cc1ad
seonhannn <seon.hannn@gmail.com>
2022-11-03 22:03:52 +0900

    레벨1 푸드파이트

---
+++ p1.js
@@ -0,0 +1,29 @@
+function solution(food) {
+    let ans = [];
+
+    for(let i=0; i<food.length; i++) {
+        if(food[i]%2) { // 홀수
+            if(food[i] === 3) {
+                for(let j=0; j<1; j++) {
+                    ans.push(i);
+                }
+            } else {
+                for(let j=0; j<(food[i]-1)/2; j++) {
+                    ans[j] = i;
+                }
+            }
+        } else {
+            for(let j=0; j<food[i]/2; j++) {
+                ans.push(i);
+            }
+        }
+    }
+
+    let reverseAns = [...ans].reverse();
+
+    ans.push(0);
+    ans = ans.join("");
+    reverseAns = reverseAns.join("");
+
+    return ans+reverseAns;
+}
