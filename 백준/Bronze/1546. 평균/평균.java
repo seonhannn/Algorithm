ca8690840ddcd7cf7619fab1e4ffc52a5d4aae66
한지선 <74178322+seonhannn@users.noreply.github.com>
2024-04-30 14:13:38 +0900

    [Bronze I] Title: 평균, Time: 268 ms, Memory: 19196 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -4,16 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int[] scores = new int[N];
-        for(int i=0; i<N; i++) {
-            scores[i] = sc.nextInt();
-        }
 
         double sumScore = 0;
         double maxScore = 0;
         for(int i=0; i<N; i++) {
-            if(scores[i] > maxScore) maxScore = scores[i];
-            sumScore += scores[i];
+            int score = sc.nextInt();
+            if(score > maxScore) maxScore = score;
+            sumScore += score;
         }
 
         System.out.println(sumScore  * 100.0 / maxScore / N);
